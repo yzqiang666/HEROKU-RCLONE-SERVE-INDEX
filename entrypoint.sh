51455d654eff77b8858e6dc14413d4cfedf21dec
yzqiang666 <yzqiang6@gmail.com>
2021-06-02 17:09:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ UU=""
 [ ! "$PASSWORD" == "" ] && UU=$UU" --pass $PASSWORD "
 
 rclone serve  webdav $CLOUDNAME:$CLOUDPATH --addr :$PORT $UU --vfs-read-chunk-size 128M &
-rclone serve  webdav $CLOUDNAME: --addr :$TLS $UU --vfs-read-chunk-size 128M &
+rclone serve  webdav $CLOUDNAME: --addr :$TLS  --vfs-read-chunk-size 128M &
