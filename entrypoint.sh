3cd16d132d59d7395e75b22c44daf25600dd1da6
yzqiang666 <yzqiang6@gmail.com>
2021-06-04 11:58:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#author https://github.com/developeranaz (don't delete this)
+rclone version
+rclone listremotes
+#refreshing Rclone
 wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
+rclone version
+rclone listremotes
 UU=""
 [  "$CLOUDPATH" == "none" ] && CLOUDPATH=""
 [  "$USER" == "none" ] && USER=""
@@ -7,4 +13,3 @@ UU=""
 [ ! "$USER" == "" ] && UU=$UU" --user $USER"
 [ ! "$PASSWORD" == "" ] && UU=$UU" --pass $PASSWORD"
 rclone serve  webdav $CLOUDNAME:$CLOUDPATH --addr :$PORT $UU --vfs-read-chunk-size 128M 
-
