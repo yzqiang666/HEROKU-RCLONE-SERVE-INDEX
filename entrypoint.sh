bfd4df17378b4bc1c01e71d6b3b1af10ba28342f
yzqiang666 <yzqiang6@gmail.com>
2021-06-04 11:45:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
-#author https://github.com/developeranaz (don't delete this)
-rclone version
-rclone listremotes
-#refreshing Rclone
 wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
-rclone version
-rclone listremotes
 UU=""
 [  "$CLOUDPATH" == "none" ] && CLOUDPATH=""
 [  "$USER" == "none" ] && USER=""
 [  "$PASSWORD" == "none" ] && PASSWORD=""
 [ ! "$USER" == "" ] && UU=$UU" --user $USER"
 [ ! "$PASSWORD" == "" ] && UU=$UU" --pass $PASSWORD "
-modprobe fuse
-rclone mount $CLOUDNAME:$CLOUDPATH /opt \
---copy-links \
---no-gzip-encoding \
---no-check-certificate \
---umask 0000 \
---default-permissions \
---allow-non-empty \
---allow-other \
---buffer-size 32M \
---dir-cache-time 12h \
---vfs-read-chunk-size 64M \
---vfs-read-chunk-size-limit 128M &
+#modprobe fuse
+#rclone mount $CLOUDNAME:$CLOUDPATH /opt \
+#--copy-links \
+#--no-gzip-encoding \
+#--no-check-certificate \
+#--umask 0000 \
+#--default-permissions \
+#--allow-non-empty \
+#--allow-other \
+#--buffer-size 32M \
+#--dir-cache-time 12h \
+#--vfs-read-chunk-size 64M \
+#--vfs-read-chunk-size-limit 128M &
 
 rclone serve  webdav $CLOUDNAME:$CLOUDPATH --addr :$PORT $UU --vfs-read-chunk-size 128M 
 
